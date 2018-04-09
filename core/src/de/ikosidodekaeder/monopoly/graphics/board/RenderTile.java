c6c40bc2d16af027e626cb959a1089cd50b2dd4d
svdragster <svdragster@gmail.com>
2018-04-09 14:18:16 +0200

    stuff

--- core/src/de/ikosidodekaeder/monopoly/graphics/board/RenderTile.java
+++ core/src/de/ikosidodekaeder/monopoly/graphics/board/RenderTile.java
@@ -31,37 +31,29 @@ public class RenderTile extends UiImage {
     }
 
     public Texture setColor(String path, Color color) {
-        SpriteBatch spriteBatch = new SpriteBatch();
-
         Texture texture = new Texture(Gdx.files.internal(path));
-        Texture texture2 = new Texture(Gdx.files.internal(path));
 
         if (!texture.getTextureData().isPrepared()) {
             texture.getTextureData().prepare();
         }
         Pixmap pixmap = texture.getTextureData().consumePixmap();
 
-        spriteBatch.begin();
 
         for (int x=0; x<getWidth(); x++) {
             for (int y=0; y<getHeight(); y++) {
 
                 Color c = new Color(pixmap.getPixel(x, y));
-                System.out.println(x + " // " + y + " ---- " + c.toString());
                 if (c.r*255 == 255
                         && c.g*255 == 0
                         && c.b*255 == 233) {
                     //spriteBatch.setColor(color);
                     //spriteBatch.draw(texture, x, y);
-                    System.out.println("#### drawing " + x + ", " + y + " -> " + color.toString());
                     pixmap.setColor(color);
                     pixmap.fillRectangle(x, y, 1, 1);
                     //texture2.draw(pixmap, x, y);
                 }
             }
         }
-        spriteBatch.end();
-        spriteBatch.dispose();
 
         Texture tex = new Texture(pixmap);
         texture.getTextureData().disposePixmap();
