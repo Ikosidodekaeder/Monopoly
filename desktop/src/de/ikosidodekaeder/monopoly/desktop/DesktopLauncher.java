cdc482039e1d918fbad53760c1b9d0a2f037b427
svdragster <svdragster@gmail.com>
2018-04-09 08:40:08 +0200

    init

---
+++ desktop/src/de/ikosidodekaeder/monopoly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.ikosidodekaeder.monopoly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.ikosidodekaeder.monopoly.Monopoly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Monopoly(), config);
+	}
+}
